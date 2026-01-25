4bf0510e78be318ff90ae09c1111a79c84ce0d8f
demouser <om@gmail.com>
2026-01-26 02:09:14 +0530

    WIP: Navbar and sidebar setup

---
+++ src/assets/index.js
@@ -0,0 +1,11 @@
+import logo from './company_logo.svg'
+import close_icon from './close_icon.svg'
+import menu_bar  from './Menu_icon.svg'
+export const company_logos = [
+ logo,
+]
+export const assets={
+
+close_icon,
+menu_bar,
+};
