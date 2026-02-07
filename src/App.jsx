4ce70e2f007a979143831b2c3e72849924e63a0f
demouser <om@gmail.com>
2026-02-08 00:04:24 +0530

    initial phase of navbar complete

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,9 @@ import Navbar from './components/Navbar'
 const App = () => {
   return (
     <div >
+      
       <Navbar/>
+     
       </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -35,13 +35,11 @@ const Navbar = () => {
     max-sm:fixed max-sm:top-16 max-sm:left-0
     max-sm:w-full max-sm:overflow-hidden
     max-sm:flex-col
-    max-sm:bg-blue-700 max-sm:text-white
+    max-sm:bg-emerald-800 max-sm:text-white
     max-sm:pt-20
   `}
 >
-
-         
-      
+   
             <a href="#" className="sm:hover:border-b">Home</a>
         <a href="#" className="sm:hover:border-b">About</a>
         <a href="#services" className="sm:hover:border-b">Team</a>
@@ -58,10 +56,24 @@ const Navbar = () => {
     className="w-8 md:hidden cursor-pointer"
   />
 
- 
-  <button className="hidden md:block bg-green-700 text-white px-4 py-2 rounded-full hover:bg-green-800 transition-colors">
-    Connect
-  </button>
+ <button className="hidden md:flex items-center gap-2 bg-emerald-600 text-white px-4 py-3 rounded-full hover:bg-emerald-700 transition-colors">
+  Connect
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className="w-5 h-5"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3"
+    />
+  </svg>
+</button>
+
 </div>
 
 
