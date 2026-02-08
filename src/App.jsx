aabdd57a71676fe6b0ddbaae2c7d281b0bb83723
demouser <om@gmail.com>
2026-02-08 13:02:29 +0530

    create responsive footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-
+import Footer from './components/Footer'
 
 const App = () => {
   return (
-    <div >
+    <div >    
       
-      <Navbar/>
-     
+       <div className="flex flex-col min-h-screen">
+      <Navbar /> {/* fixed navbar */}
+
+      {/* Main content */}
+      <main className="flex-1 pt-16 pb-8">
+        <h1 className="text-3xl font-bold mb-4">Page 1</h1>
+        <p className='font-bold text-lg items-center'>update is coming..........</p>
+      </main>
+
+      <Footer />
+    </div>
       </div>
   )
 }
